656ae1e09b11fd72e8065d958c81f9dc1eb0b029
Arzaroth Lekva <lekva@arzaroth.com>
2014-12-27 00:40:15 +0900

    spliting files

---
+++ src/currenciesframe.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File: currenciesframe.py
+# by Arzaroth Lekva
+# arzaroth@arzaroth.com
+#
+
+from collections import OrderedDict
+from tkinter import Label, Entry, Frame, StringVar
+from tkinter.constants import N, S, E, W, NSEW
+from src.defaultordereddict import DefaultOrderedDict
+
+class CurrencyFrame(object):
+
+    def __init__(self, parent, text, value, offset):
+        self.parent = parent
+
+        self._value = StringVar(self.parent, value)
+
+        self._label = Label(self.parent, text=text)
+        self._entry = Entry(self.parent, textvariable=self._value)
+
+        options = dict(padx=10, pady=4)
+        self._label.grid(row=offset, column=0, sticky=W, **options)
+        self._entry.grid(row=offset, column=1, sticky=E, **options)
+
+    @property
+    def value(self):
+        return self._value.get()
+
+
+class CurrenciesFrame(Frame):
+
+    def __init__(self, parent, xml_handle):
+        super(CurrenciesFrame, self).__init__(parent)
+
+        self._xml_handle = xml_handle
+        self._currencies = DefaultOrderedDict(OrderedDict)
+        n = 0
+        for name, typ in xml_handle.currencies.items():
+            for cur, val in typ.items():
+                self._currencies[name][cur] = CurrencyFrame(self, cur, val.value, n)
+                n += 1
+
+    def commit(self):
+        for name, typ in xml_handle.currencies.items():
+            for cur, val in typ.items():
+                val.value = self._currencies[name][cur].value
--- src/ponygui.py
+++ src/ponygui.py
@@ -6,60 +6,21 @@
 # arzaroth@arzaroth.com
 #
 
-from collections import OrderedDict
-from tkinter import Label, Button, Entry, StringVar, Frame, Toplevel
+from tkinter import Label, Button, Entry, Frame, Toplevel
 from tkinter.ttk import Notebook
 from tkinter.filedialog import askopenfilename
 from tkinter.constants import N, S, E, W, NSEW
-from tkinter.messagebox import showerror
 from src.basegui import BaseGui
+from src.currenciesframe import CurrenciesFrame
 from src import SaveManager, SaveError
 from src import decompress_data, compress_data
 from src import XmlHandler
-from src.defaultordereddict import DefaultOrderedDict
-
-class CurrencyFrame(Frame):
-
-    def __init__(self, parent, text, value):
-        super(CurrencyFrame, self).__init__(parent)
-
-        self._value = StringVar(self, value)
-
-        self._label = Label(self, text=text)
-        self._entry = Entry(self, textvariable=self._value)
-
-        options = dict(sticky=NSEW, padx=3, pady=4)
-        self._label.grid(row=0, column=0, **options)
-        self._entry.grid(row=0, column=1, **options)
-
-    @property
-    def value(self):
-        return self._value.get()
-
-
-class CurrenciesFrame(Frame):
-
-    def __init__(self, parent, xml_handle):
-        super(CurrenciesFrame, self).__init__(parent)
-
-        self._xml_handle = xml_handle
-        self._currencies = DefaultOrderedDict(OrderedDict)
-        for name, typ in xml_handle.currencies.items():
-            for cur, val in typ.items():
-                self._currencies[name][cur] = CurrencyFrame(self, cur, val.value)
-                self._currencies[name][cur].grid()
-
-    def commit(self):
-        for name, typ in xml_handle.currencies.items():
-            for cur, val in typ.items():
-                val.value = self._currencies[name][cur].value
-
 
 class LoadingDialog(Toplevel):
 
     def __init__(self, parent):
         super(LoadingDialog, self).__init__()
-        Label(self, text="Loading...").pack()
+        Label(self, text="Loading...", font=25, height=3, width=25).pack()
 
 
 class PonyGui(BaseGui):
